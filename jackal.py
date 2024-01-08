9038a9207183727db16204d61ba652668b4b9f14
Cakuges <93228791+cakuge@users.noreply.github.com>
2024-01-09 01:05:10 +0300

    Update jackal.py

--- jackal.py
+++ jackal.py
@@ -14,32 +14,34 @@ def main():
      print(colorama.Fore.BLUE + metin + colorama.Fore.RESET)
 
     sekilli_yazi("""
-__________________________________________
-/_____/_____/_____/_____/_____/_____/_____/
-     __               __           .__     
-    |__|____    ____ |  | _______  |  |    
-    |  \__  \ _/ ___\|  |/ /\__  \ |  |    
-    |  |/ __ \  \___|    <  / __ \|  |__  
-/\__|  (____  /\___  >__|_ \(____  /____/  
-\______|    \/     \/     \/     \/        
-                                                    
- __________________________________________
-/_____/_____/_____/_____/_____/_____/_____/
-""")
-    print(colorama.Fore.YELLOW + "Hoşgeldiniz, aktive etmeden önce lütfen readme.txt yi okuyunuz." + colorama.Fore.RESET)
-    print(colorama.Fore.YELLOW + "Devam etmek için [y] tuşuna basın ya da [n] yazın." + colorama.Fore.RESET)
+    __________________________________________
+    /_____/_____/_____/_____/_____/_____/_____/
+        __               __           .__     
+        |__|____    ____ |  | _______  |  |    
+        |  \__  \ _/ ___\|  |/ /\__  \ |  |    
+        |  |/ __ \  \___|    <  / __ \|  |__  
+    /\__|  (____  /\___  >__|_ \(____  /____/  
+    \______|    \/     \/     \/     \/        
+                                                        
+    __________________________________________
+    /_____/_____/_____/_____/_____/_____/_____/
+    """)
+    print(colorama.Fore.YELLOW + "* Welcome, please readme first read readme.txt" + colorama.Fore.RESET)
+    print(colorama.Fore.YELLOW + "* If you want to add some more special things, you can edit pws.txt file." + colorama.Fore.RESET)
+    print(colorama.Fore.YELLOW + "* Enter tags of at least 3 characters." + colorama.Fore.RESET)
+    print(colorama.Fore.YELLOW + "* Press the [y] key to continue, if you do not want to continue, type [n]." + colorama.Fore.RESET)
     devam = input()
     if devam == "y":
 
         girdiler = []
         while True:
-            girdi = input(colorama.Fore.RED + "Etiket Giriniz: " + colorama.Fore.RESET)
+            girdi = input(colorama.Fore.RED + "Enter Tag: " + colorama.Fore.RESET)
             girdiler.append(girdi)
-            print(colorama.Fore.RED + "Girilen etiketler:", colorama.Fore.GREEN + ", ".join(girdiler) + colorama.Fore.RESET)
-            devam = input("Devam etmek için (y) durdurmak için (n) giriniz: ")
+            print(colorama.Fore.RED + "Entered Tags:", colorama.Fore.GREEN + ", ".join(girdiler) + colorama.Fore.RESET)
+            devam = input(colorama.Fore.YELLOW + "Press the [y] key to continue, if you do not want to continue, type [n]. " + colorama.Fore.RESET)
             if devam == "n":
-                print(colorama.Fore.YELLOW + "Dosyanız oluşturuldu." + colorama.Fore.RESET)
-               
+                print(colorama.Fore.YELLOW + "Your files is created." + colorama.Fore.RESET)
+                print(colorama.Fore.YELLOW + "Good Luck." + colorama.Fore.RESET)
                 break
 
         with open("pws.txt", "r") as f:
@@ -74,11 +76,12 @@ __________________________________________
         dosya_yolu = os.path.join(os.getcwd(), "passwords.txt")
         print(os.path.dirname(dosya_yolu))               
     else:
-        print(colorama.Fore.RED + "Çıkış Yapılıyor." + colorama.Fore.RESET)
+        print(colorama.Fore.RED + "## Exit ##" + colorama.Fore.RESET)
         devam == False
         return
     os.system("python controller.py")
-    print("Dosyanız Oluşturuldu.")
-    print(colorama.Fore.GREEN + "Çıkış Yapıldı." + colorama.Fore.RESET)
+    print("Your file is created.")
+    print(colorama.Fore.GREEN + "### Exit ###" + colorama.Fore.RESET)
+    print(colorama.Fore.YELLOW + "Good Luck." + colorama.Fore.RESET)
 if __name__ == "__main__":
      main()
